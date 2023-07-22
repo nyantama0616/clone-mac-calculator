a345cd45faf457692195cca449e5a9117892a604
nyantama0616 <r_nyantama0616@yahoo.co.jp>
2023-07-23 00:48:25 +0900

    演算子以外のロジックはできた

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -23,7 +23,7 @@ export function Home() {
                 <Calculator/>
             </AspectRatio>
             {/* </Container> */}
-            <GridPractice />
+            {/* <GridPractice /> */}
         </div>
     )
 }
--- src/hooks/providers/CalculatorProvider.tsx
+++ src/hooks/providers/CalculatorProvider.tsx
@@ -14,15 +14,54 @@ export default function CalculatorProvider({ children }: CalculatorProviderProps
 }
 
 type CalcReturnValue = {
-    // leftValue: string
-    // rightValue: string
     displayValue: string
     sendValue: (value: string) => void
 };
 function useCalculator(): CalcReturnValue {
-    const [leftValue, setLeftValue] = useState<string>("0");
-    const [rightValue, setRightValue] = useState<string>("0");
+    const [leftValue, setLeftValue] = useState<string>("");
+    const [rightValue, setRightValue] = useState<string>("");
     const [operator, setOperator] = useState<Function>(() => { });
+    const [displayValue, setDisplayValue] = useState<string>("0");
+
+    function allClear() {
+        setLeftValue("");
+        setRightValue("");
+        setDisplayValue("0");
+    }
+
+    function plusMinus() {
+        const value = parseFloat(rightValue);
+        if (!Number.isNaN(value)) {
+            const newValue = (-1 * value).toString();
+            setRightValue(newValue);
+            setDisplayValue(newValue);
+        }
+    }
+
+    function percent() {
+        const value = parseFloat(rightValue);
+        if (!Number.isNaN(value)) {
+            const newValue = (0.01 * value).toString();
+            setRightValue(newValue);
+            setDisplayValue(newValue);
+        }
+    }
+
+    function dot() {
+        const count = (rightValue.match(/\./g) || []).length;
+        if (count > 0) return;
+        
+        const value = parseFloat(rightValue);
+        let newValue;
+        if (!Number.isNaN(value)) {
+            newValue = rightValue + ".";
+        } else {
+            newValue = "0.";
+        }
+
+        setRightValue(newValue);
+        setDisplayValue(newValue);
+    }
 
     function divide() {
 
@@ -44,10 +83,28 @@ function useCalculator(): CalcReturnValue {
 
     }
 
-    const displayValue = leftValue;
-    function sendValue(value: string) {
-        console.log(`send!: ${value}`);
-        setLeftValue(value);
+    function sendValue(value: string) {        
+        if (!Number.isNaN(parseFloat(value))) { //valueが数値の場合
+            const newValue = rightValue + value;
+            setRightValue(newValue);
+            setDisplayValue(newValue);
+            return;
+        }
+
+        switch (value) {
+            case "AC":
+                allClear();
+                break;
+            case "+/-":
+                plusMinus();
+                break;
+            case "%":
+                percent();
+                break;
+            case ".":
+                dot();
+                break;
+        }
     }
 
     return { displayValue, sendValue };
