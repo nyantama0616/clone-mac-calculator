5d1cf8494d0bc88f2d6cff80df5d2e10d60664c5
nyantama0616 <r_nyantama0616@yahoo.co.jp>
2023-07-23 22:24:33 +0900

    NumberButtons, OperatorButtons, OtherButtons, すべてのボタンのロジックが完成した
    「=」を押した後に「+/-」ボタンなどを押すと、挙動がおかしいので要修正

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import Calculator from '../compounds/Calculator';
-// import { Container } from '@mui/material';
-// import { AspectRatio } from '@mui/icons-material';
 import { AspectRatio } from '@mui/joy';
 import GridPractice from '../compounds/GridPractice';
+import { Grid } from '@mui/material';
 
 export function Home() {
     return (
         <div className="home">
             <h1>home</h1>
 
-            {/* <Container sx={{ py: 8 }} maxWidth="md" fixed> */}
-            <AspectRatio
-                variant="outlined"
-                ratio="3/4"
-                sx={{
-                    width: 500,
-                    bgcolor: 'background.level2',
-                    borderRadius: 'md',
-                }}
+            <Grid
+                container
+                justifyContent={"center"}
+                alignItems={"center"}
+                height={1000} //ここを親要素の100%にしたい
             >
-                <Calculator/>
-            </AspectRatio>
-            {/* </Container> */}
-            {/* <GridPractice /> */}
+                <AspectRatio
+                    variant="outlined"
+                    ratio="3/4"
+                    sx={{
+                        width: 500,
+                        bgcolor: 'background.level2',
+                        borderRadius: 'md',
+                    }}
+                    >
+                    <Calculator/>
+                </AspectRatio>
+            </Grid>
         </div>
     )
 }
--- src/hooks/providers/CalculatorProvider.tsx
+++ src/hooks/providers/CalculatorProvider.tsx
@@ -1,6 +1,6 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 
-export const CalculatorContext = createContext<CalcReturnValue>({displayValue: "", sendValue: () => {}});
+export const CalculatorContext = createContext<CalcReturnValue>({state: {left: "", right: "", displayValue: "", operator: () => {}}, sendValue: () => {}});
 
 type CalculatorProviderProps = {
     children: React.ReactNode
@@ -13,99 +13,208 @@ export default function CalculatorProvider({ children }: CalculatorProviderProps
     </CalculatorContext.Provider>
 }
 
-type CalcReturnValue = {
+type CalcState = {
+    left: string
+    right: string
     displayValue: string
+    operator: () => void
+};
+type CalcReturnValue = {
+    state: CalcState
     sendValue: (value: string) => void
 };
 function useCalculator(): CalcReturnValue {
-    const [leftValue, setLeftValue] = useState<string>("");
-    const [rightValue, setRightValue] = useState<string>("");
-    const [operator, setOperator] = useState<Function>(() => { });
-    const [displayValue, setDisplayValue] = useState<string>("0");
+    const initialOperator = () => {
+        console.log("init!");
+        setState(prev => {
+            const newState = { ...prev };
+            newState.left = prev.right;
+            newState.right = "";
+            return newState;
+        });
+    };
+
+    const initialState = {
+        left: "",
+        right: "",
+        displayValue: "0",
+        operator: initialOperator,
+    };
+
+    const [state, setState] = useState<CalcState>(initialState);
 
     function allClear() {
-        setLeftValue("");
-        setRightValue("");
-        setDisplayValue("0");
+        // console.log("all clear");
+        setState(initialState);
     }
 
     function plusMinus() {
-        const value = parseFloat(rightValue);
-        if (!Number.isNaN(value)) {
+        setState(prev => {
+            const value = parseFloat(prev.right);
+            if (Number.isNaN(value)) return prev; //rightが空文字列の場合を考慮
             const newValue = (-1 * value).toString();
-            setRightValue(newValue);
-            setDisplayValue(newValue);
-        }
+            const newState = { ...state };
+            newState.right = newValue;
+            newState.displayValue = newValue;
+            return newState;
+        });
     }
 
     function percent() {
-        const value = parseFloat(rightValue);
+        const value = parseFloat(state.right);
         if (!Number.isNaN(value)) {
             const newValue = (0.01 * value).toString();
-            setRightValue(newValue);
-            setDisplayValue(newValue);
+            const newState = state;
+            newState.right = newValue;
+            newState.displayValue = newValue;
+            setState(newState);
         }
     }
 
     function dot() {
-        const count = (rightValue.match(/\./g) || []).length;
+        const count = (state.right.match(/\./g) || []).length;
         if (count > 0) return;
-        
-        const value = parseFloat(rightValue);
+
+        const value = parseFloat(state.right);
         let newValue;
         if (!Number.isNaN(value)) {
-            newValue = rightValue + ".";
+            newValue = state.right + ".";
         } else {
             newValue = "0.";
         }
 
-        setRightValue(newValue);
-        setDisplayValue(newValue);
+        const newState = state;
+        newState.right = newValue;
+        newState.displayValue = newValue;
+        setState(newState);
     }
 
     function divide() {
+        setState(prev => {
+            const leftValueParsed = parseFloat(prev.left);
+            const rightValueParsed = parseFloat(prev.right);
+            // if (rightValueParsed === 0) return prev; //いる？
 
+            const newValue = (leftValueParsed / rightValueParsed).toString();
+            const newState = { ...prev };
+            newState.left = newValue;
+            newState.right = "";
+            newState.displayValue = newValue;
+            return newState;
+        });
     }
 
     function multiply() {
+        setState(prev => {
+            const leftValueParsed = parseFloat(prev.left);
+            const rightValueParsed = parseFloat(prev.right);
 
+            const newValue = (leftValueParsed * rightValueParsed).toString();
+            const newState = { ...prev };
+            newState.left = newValue;
+            newState.right = "";
+            newState.displayValue = newValue;
+            return newState;
+        });
     }
 
     function minus() {
+        setState(prev => {
+            const leftValueParsed = parseFloat(prev.left);
+            const rightValueParsed = parseFloat(prev.right);
 
+            const newValue = (leftValueParsed - rightValueParsed).toString();
+            const newState = { ...prev };
+            newState.left = newValue;
+            newState.right = "";
+            newState.displayValue = newValue;
+            return newState;
+        });
     }
 
     function plus() {
+        setState(prev => {
+            const leftValueParsed = parseFloat(prev.left);
+            const rightValueParsed = parseFloat(prev.right);
 
+            const newValue = (leftValueParsed + rightValueParsed).toString();
+            const newState = { ...prev };
+            newState.left = newValue;
+            newState.right = "";
+            newState.displayValue = newValue;
+            return newState;
+        });
     }
 
     function equal() {
-
+        setState(prev => {
+            const newState = { ...prev };
+            newState.left = prev.right;
+            newState.right = "";
+            newState.displayValue = "";
+            return newState;
+        });
     }
+
+    function appendNum(value: String) {
+        setState(prev => {
+            const newValue = state.right + value;
+            const newState = { ...prev };
+            newState.right = newValue;
+            newState.displayValue = newValue;
+            return newState
+        });
+    }
+
+    function setOperator(operator: () => void) {
+        state.operator();
+        setState(prev => {
+            const newState = { ...prev };
+            newState.operator = operator;
+            return newState;
+        });
+    }
+
+    useEffect(() => {
+        console.log(`left: ${state.left}, right: ${state.right}, disPlay: ${state.displayValue}`);
+    }, [state]);
 
     function sendValue(value: string) {        
         if (!Number.isNaN(parseFloat(value))) { //valueが数値の場合
-            const newValue = rightValue + value;
-            setRightValue(newValue);
-            setDisplayValue(newValue);
+            appendNum(value);
             return;
         }
 
         switch (value) {
-            case "AC":
-                allClear();
-                break;
-            case "+/-":
-                plusMinus();
-                break;
-            case "%":
-                percent();
-                break;
-            case ".":
-                dot();
-                break;
+        case "AC":
+            allClear();
+            break;
+        case "+/-":
+            plusMinus();
+            break;
+        case "%":
+            percent();
+            break;
+        case ".":
+            dot();
+            break;
+        case "÷":
+            setOperator(divide);
+            break;
+        case "✕":
+            setOperator(multiply);
+            break;
+        case "-":
+            setOperator(minus);
+            break;
+        case "+":
+            setOperator(plus);
+            break;
+        case "=":
+            setOperator(() => { });
+            break;
         }
     }
 
-    return { displayValue, sendValue };
+    return { state, sendValue };
 }
